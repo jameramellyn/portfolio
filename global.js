bbf1bb668ccf7cad1af3043cb69790123d456e58
jameramellyn <107943103+jameramellyn@users.noreply.github.com>
2026-04-17 16:21:37 -0700

    light_dark_mode

--- global.js
+++ global.js
@@ -1,5 +1,97 @@
-console.log('IT’S ALIVE!');
+console.log('IT\’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/jameramellyn', title: 'Github' },
+  ];
+
+const nav = document.createElement('nav');
+document.body.prepend(nav); 
+
+const BASE_PATH =
+  location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "/"
+    : "/portfolio/";
+
+    for (let p of pages) {
+        let url = p.url;
+        let title = p.title;
+      
+        url = !url.startsWith('http') ? BASE_PATH + url : url;
+      
+        let a = document.createElement('a');
+        a.href = url;
+        a.textContent = title;
+      
+        a.classList.toggle(
+          'current',
+          a.host === location.host && a.pathname === location.pathname
+        );
+      
+        if (a.host !== location.host) {
+          a.target = "_blank";
+        }
+      
+        nav.append(a);
+      }    
+// ==========================
+// THEME SWITCHER UI
+// ==========================
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+    `
+  );
+  
+  const select = document.querySelector('.color-scheme select');
+  
+  function setColorScheme(scheme) {
+    document.documentElement.style.setProperty('color-scheme', scheme);
+  }
+  
+  // ==========================
+  // EVENT LISTENER
+  // ==========================
+  
+  select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+  
+    setColorScheme(value);
+  
+    localStorage.colorScheme = value;
+  });
+  
+  
+  if ('colorScheme' in localStorage) {
+    const saved = localStorage.colorScheme;
+  
+    setColorScheme(saved);
+    select.value = saved;
+  }
+
+// const $$ = (selector, context = document) =>
+//     Array.from(context.querySelectorAll(selector));
+  
+//   const navLinks = $$("nav a");
+  
+//   let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+//   );
+  
+//   currentLink?.classList.add('current');
