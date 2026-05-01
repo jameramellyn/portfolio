c6f7d9c7bba0b41a182ea5b40d43a9106a942a88
jameramellyn <107943103+jameramellyn@users.noreply.github.com>
2026-05-01 14:00:15 -0700

    circle

--- global.js
+++ global.js
@@ -125,6 +125,8 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
+      <p class="project-year">${project.year}</p>
+      </div>
     `;
 
     containerElement.appendChild(article);
--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,20 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects-grid');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(10);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('#projects-pie-plot')
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
